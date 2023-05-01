abb1d30f27ef2f46687bbe073c0e0a9a0034779e
Demi Alabi <demialabi98@gmail.com>
2023-05-01 11:44:49 -0400

    functions-101

---
+++ exercises/functions/simple-functions.js
@@ -0,0 +1,16 @@
+function favoriteThings() {
+  console.log("Demi's favorite things are: ");
+  console.log("My favorite movie right now is Dune .");
+  console.log("My favorite band is A Tribe Called Quest.");
+}
+
+favoriteThings();
+
+
+function whyImHere(){
+console.log(`I want to create a new foundation for success.`);
+}
+
+whyImHere();
+
+
